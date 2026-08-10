d7114c9c0a212b958421ccae2b014a86529bf242
Ignatius-ii <ignaelliot@gmail.com>
2026-08-10 10:47:15 +0300

    Enable CORS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 import auth
 import users
@@ -12,6 +12,14 @@ app = FastAPI(
     title="Lenovo Support - Auth Service",
     description="Full CRUD authentication with admin, staff, and customer roles",
     version="1.0.0",
+)
+
+app.add_middleware(
+CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
